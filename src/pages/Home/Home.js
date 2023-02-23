683e0cbee386c8cb7c36b7398f69716439d4d506
hristoz2020 <62563444+hristoz2020@users.noreply.github.com>
2023-02-23 19:55:34 +0200

    add photo to home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,7 +2,10 @@
 const Home = () => {
 
     return(
-        <h1>Home Page!</h1>
+        <>
+        <h1 className="home-page-welcome">Welcome to car app</h1>
+        <img src="https://hips.hearstapps.com/hmg-prod/amv-prod-cad-assets/images/16q4/671589/are-smartphone-apps-making-cars-more-convenient-features-car-and-driver-photo-672890-s-original.jpg?resize=1200:*" alt="cars" className="home-page-img" />
+        </>
     );
 };
 
